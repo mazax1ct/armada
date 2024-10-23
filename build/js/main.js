1f5d5a9705b329c6355da6ea75b36f97c0e32a05
mazax1ct <vova@giveandget.ru>
2024-10-23 13:12:26 +0300

    addordion + coaches

--- build/js/main.js
+++ build/js/main.js
@@ -119,3 +119,9 @@ $(document).ready(function() {
      }
   });
 });
+
+$(document).on('click', '.accordion__toggler', function () {
+  $(this).toggleClass('is-active');
+  $(this).closest('.accordion').find('.accordion__body').slideToggle();
+  return false;
+});
